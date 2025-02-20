167089a3cfbcf36446cdf54325eb31430d45b29f
N-Berns <n.berns@protonmail.com>
2025-02-20 17:08:23 +0800

    Added error handling for 'Invalid pokemon name'

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -22,9 +22,6 @@ router.get("/pokemon/:name", async (req, res) => {
     try {
         const { name } = req.params
         const response = await axios.get(`${API_URL}/${name}`)
-
-        if (!response.data) return res.render("pages/noName")
-
         return res.render("pages/details", {pokemon: response.data})
     } catch (error) {
         console.error("Error fetching pokemon ", error)
@@ -37,10 +34,27 @@ router.get("/search", async (req, res) => {
     try {
         const { name } = req.query;
         if (!name) return res.redirect("/")
-        const response = await axios.get(`${API_URL}/${name.toLowerCase()}`)
-        return res.render("pages/details", {pokemon: response.data})
+        
+            try {
+                const response = await axios.get(`${API_URL}/${name.toLowerCase()}`)
+                return res.render("pages/details", {pokemon: response.data}) 
+            } catch (error) {
+                return res.render("pages/noName")
+            }
+
     } catch (error) {
-        console.error("Error searching pokemon ", error)
+        console.error("Error searching pokemon")
+        return res.status(500).json({message: "Internal Server Error"})
+    }
+})
+
+// No pokemon of such name
+router.get("/noName", async (req, res) => {
+    try {
+        const response = await axios.get(`${API_URL}?limit=20`)
+        return res.render("pages/home", {pokemonList: response.data.results})
+    } catch (error) {
+        console.error("Error fetching pokemon list " , error)
         return res.status(500).json({message: "Internal Server Error"})
     }
 })
