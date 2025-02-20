7fbb3c288e04e271015ec54d903e5aee1efa306f
N-Berns <n.berns@protonmail.com>
2025-02-20 22:32:18 +0800

    Tailwind Styling

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -10,10 +10,10 @@ const API_URL = process.env.API_URL
 router.get("/", async (req, res) => {
     try {
         const response = await axios.get(`${API_URL}?limit=20`)
-        return res.render("pages/home", {pokemonList: response.data.results})
+        return res.render("pages/home", { pokemonList: response.data.results })
     } catch (error) {
-        console.error("Error fetching pokemon list " , error)
-        return res.status(500).json({message: "Internal Server Error"})
+        console.error("Error fetching pokemon list ", error)
+        return res.status(500).json({ message: "Internal Server Error" })
     }
 })
 
@@ -22,10 +22,10 @@ router.get("/pokemon/:name", async (req, res) => {
     try {
         const { name } = req.params
         const response = await axios.get(`${API_URL}/${name}`)
-        return res.render("pages/details", {pokemon: response.data})
+        return res.render("pages/details", { pokemon: response.data })
     } catch (error) {
         console.error("Error fetching pokemon ", error)
-        return res.status(500).json({message: "Internal Server Error"})
+        return res.status(500).json({ message: "Internal Server Error" })
     }
 })
 
@@ -34,28 +34,17 @@ router.get("/search", async (req, res) => {
     try {
         const { name } = req.query;
         if (!name) return res.redirect("/")
-        
-            try {
-                const response = await axios.get(`${API_URL}/${name.toLowerCase()}`)
-                return res.render("pages/details", {pokemon: response.data}) 
-            } catch (error) {
-                return res.render("pages/noName")
-            }
+
+        try {
+            const response = await axios.get(`${API_URL}/${name.toLowerCase()}`)
+            return res.render("pages/details", { pokemon: response.data })
+        } catch (error) {
+            return res.render("pages/noName")
+        }
 
     } catch (error) {
         console.error("Error searching pokemon")
-        return res.status(500).json({message: "Internal Server Error"})
-    }
-})
-
-// No pokemon of such name
-router.get("/noName", async (req, res) => {
-    try {
-        const response = await axios.get(`${API_URL}?limit=20`)
-        return res.render("pages/home", {pokemonList: response.data.results})
-    } catch (error) {
-        console.error("Error fetching pokemon list " , error)
-        return res.status(500).json({message: "Internal Server Error"})
+        return res.status(500).json({ message: "Internal Server Error" })
     }
 })
 
